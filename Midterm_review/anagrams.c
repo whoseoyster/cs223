ac7ba820786563715f95fc3f99dc24994bcb324a
Rishab Ramanathan <rishabramanathan@Rishabs-MacBook-Pro.local>
2017-03-23 18:14:58 -0400

    Makefile change

--- Midterm_review/anagrams.c
+++ Midterm_review/anagrams.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <math.h>
 #include <stdbool.h>
+#include <assert.h>
 
 int cmpfunc(const void *p1, const void *p2) {
 
@@ -28,17 +29,27 @@ int cmpfunc(const void *p1, const void *p2) {
 
 int main(int argc, char ** argv) {
   
-  char word[4] = "rut";
-  char word2[4] = "urr";
+  // char word[4] = "rut";
+  // char word2[4] = "urr";
   
-  qsort(word, strlen(word), sizeof(word[0]), cmpfunc);
-  qsort(word2, strlen(word2), sizeof(word[0]), cmpfunc);
+  // qsort(word, strlen(word), sizeof(word[0]), cmpfunc);
+  // qsort(word2, strlen(word2), sizeof(word[0]), cmpfunc);
 
-  if (strcmp(word, word2) == 0) {
-    printf("yes\n");
-  } else {
-    printf("no\n");
-  }
+  // if (strcmp(word, word2) == 0) {
+  //   printf("yes\n");
+  // } else {
+  //   printf("no\n");
+  // }
+
+  char *foo;
+  foo = malloc(20);
+  assert(foo);
+  printf("1 %s\n", foo);
+  memset(foo, 'x', 22);
+  printf("2 %s\n", foo);
+  strncpy(foo, "you are", 7);
+  printf("3 %s\n", foo);
+  free(foo);
 
   return 0;
 }
