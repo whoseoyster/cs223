bfc75de4e319c307350b314468479561141117ed
Rishab Ramanathan <rishabramanathan@Rishabs-MBP.wireless.yale.internal>
2017-03-25 01:38:25 -0400

    bf

--- hw4/stack.c
+++ hw4/stack.c
@@ -52,11 +52,12 @@ StackDestroy(stackT *stackP)
 {
 	/* Get rid of array. */
 	for (int i=0; i<stackP->top+1; i++) {
+		printf("token: %s\n", contents[i]->str);
 		free(stackP->contents[i]->str);
-		free(stackP->contents[i]);	
+		// free(stackP->contents[i]);	
 	}
 
-	// free(stackP->contents);
+	free(stackP->contents);
 	
 	stackP->contents = NULL;
 	stackP->maxSize = 0;
