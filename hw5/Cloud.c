ba091619f89132a46c380e140e120447a53fe90b
Rishab Ramanathan <rishabramanathan@Rishabs-MBP.wireless.yale.internal>
2017-04-02 18:03:35 -0400

    added avl

--- hw5/Cloud.c
+++ hw5/Cloud.c
@@ -118,7 +118,7 @@ int main(int argc, char **argv) {
               aoldnode->next = acloud;
               acloud = aoldnode;
             } else {
-              atreeInsert(aroot, tok);
+              atreeInsert(&aroot, tok);
             }
           }
         } else {
