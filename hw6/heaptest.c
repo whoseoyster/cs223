42b53e23570a6728cecd8e3dde37032a74f76004
Rishab Ramanathan <rr566@python.zoo.cs.yale.edu>
2017-04-12 13:34:01 -0400

    changed heaptest

--- hw6/heaptest.c
+++ hw6/heaptest.c
@@ -3,39 +3,41 @@
 #include <stdbool.h>
 #include <limits.h>
 #include <string.h>
-#include <assert.h>
 #include "heap.h"
 #include "dict.h"
 
-// void * mymalloc(size_t s, int i);
+void * mymalloc(size_t s, int i);
+void removeRoom(Heap h) {
+  struct room * min = findmin(h);
+  free(min);
+  deletemin(h);
+}
 
 int main(int argc, char ** argv){
   debugflag = true;
+
   Heap h = initheap();
 
   struct room * r;
 
   //printHeap();
-
   if (argc > 1) {
     for (int i=1; i < argc; i++) {
       int val = atoi(argv[i]);
       if (val > 0) {
-      	r = malloc(sizeof(struct room *) * 7);
-        assert(r);
-
-      	r->temp = val*val;
-
-      	sprintf(r->room, "R%d", val);
-
-      	insert(h, val, r);
-
+	r = mymalloc(sizeof(struct room *), 7);
+	r->temp = val*val;
+	sprintf(r->room, "R%d", val);
+	insert(h, val, r);
       } else {
-	      deletemin(h);
+	removeRoom(h);
       }
     }
   } else {
     fprintf(stderr, "Usage: heap [numbers]+\n");
+  }
+  while (!empty(h)) {
+    removeRoom(h);
   }
   destroyHeap(h);
 
