23aad7f4be31d72fd96316a4a827ee61ecdd3755
Rishab Ramanathan <rishabramanathan@Rishabs-MBP.wireless.yale.internal>
2017-04-14 11:24:54 -0400

    heap silenced

--- hw6/heap.c
+++ hw6/heap.c
@@ -61,7 +61,8 @@ Heap initheap()
 void swap(heapslot *s1, heapslot *s2)
 {
 	heapslot temp;
-	printf("SWAP: %d <=> %d\n", s1->key, s2->key);
+	if (debugflag)
+		printf("SWAP: %d <=> %d\n", s1->key, s2->key);
 	temp = *s1;
 	*s1 = *s2;
 	*s2 = temp;
@@ -86,13 +87,14 @@ void insert( Heap h, int key, struct room * r)
 	//demand(heapnum < maxheap, "heap overflow\n");
 	while(h->n  >= h->size) {
 		h->size *= 2;
-		printf("\nExpanding heap size to %d\n", h->size);
+		if (debugflag)
+			printf("\nExpanding heap size to %d\n", h->size);
 		h->heap = realloc(h->heap, sizeof(*(h->heap)) * h->size);
 		demand(h != 0, "malloc fail in insert\n");
-		printf("Realloc: %p\n", (void *) h->heap);
+		if (debugflag)
+			printf("Realloc: %p\n", (void *) h->heap);
 		for (int i = (h->size)/2; i < (h->size); i++)
 	    {
-	      printf("new Heapslot: %i\n", i);
 	      h->heap[i] = malloc(sizeof(*(h->heap[i])));
 	      h->heap[i]->key = INT_MIN;
 	      h->heap[i]->value = NULL;
@@ -110,8 +112,10 @@ void insert( Heap h, int key, struct room * r)
 	parent = cur/2;
 
 	while (h->heap[parent] && h->heap[parent]->key > h->heap[cur]->key) {
-		printf("\n--");
-		printHeap(h);
+		if (debugflag) {
+			printf("\n--");
+			printHeap(h);
+		}
 		demand(parent > 0, "inserted item rising past root\n");
 		swap(h->heap[parent], h->heap[cur]);
 		cur = parent;
@@ -119,22 +123,25 @@ void insert( Heap h, int key, struct room * r)
 	}
 
 	// j->n++;
-
-	printHeap(h);
+	if (debugflag)
+		printHeap(h);
 }
 
 void deletemin( Heap h)
 {
 	int cur, child;
 	int heapnum = h->n;
-	printf("DELETEMIN: ");
+	if (debugflag)
+		printf("DELETEMIN: ");
 	demand(! empty(h), "deletemin not allowed on empty heap\n");
-	h->heap[1] = h->heap[heapnum--];
+	swap(h->heap[1], h->heap[heapnum--]);
 	cur = 1;
 	child = 2;
 	while (child <= heapnum) {
-		printf("\n--");
-		printHeap(h);
+		if (debugflag) {
+			printf("\n--");
+			printHeap(h);
+		}
 		if (child < heapnum &&
 			h->heap[child+1]->key < h->heap[child]->key)
 		  child++;
@@ -146,7 +153,8 @@ void deletemin( Heap h)
 		} else
 		  break;
 	}
-	printHeap(h);
+	if (debugflag)
+		printHeap(h);
 }
 
 void destroyHeap( Heap h)
@@ -154,218 +162,18 @@ void destroyHeap( Heap h)
 	int i;
     heapslot *e;
 
-    printf("DestroyHeap\n");
+    if (debugflag)
+    	printf("DestroyHeap\n");
 
     for(i = 0; i < h->size; i++) {
         e = h->heap[i];
-        printf("Freeing: %d\n", e->key);
-        if (e->value)
-        	free(e->value);
+        if (debugflag)
+        	printf("Freeing: %d\n", e->key);
+        // if (e->value)
+        // 	free(e->value);
         free(e);
     }
 
     free(h->heap);
     free(h);
 }
-
-// ///////////////////////////////////////////////////////////////////////////////
-// // heap.c       erc39
-// // Implements heap functions
-// ///////////////////////////////////////////////////////////////////////////////
-
-// #include <stdio.h>
-// #include "heap.h"
-// #include <stdbool.h>
-
-// struct room {
-//   char room[4];
-//   int temp;
-//   int ncount;
-//   int visited;
-//   char neighbors[10][4];
-// };
-
-// ///////////////////////////////////////////////////////////////////////////////
-// // empty
-// // Returns true if heap is empty
-// ///////////////////////////////////////////////////////////////////////////////
-// bool empty(Heap h)
-// {
-//   return h->n == 0;
-// }
-
-// ///////////////////////////////////////////////////////////////////////////////
-// // demand
-// // If the cond is false, print message and exit
-// ///////////////////////////////////////////////////////////////////////////////
-// void demand(int cond, char * msg)
-// {
-//   if (! cond)
-//   {
-//     fprintf(stderr, "%s\n", msg);
-//     exit(1);
-//   }
-//   return;
-// }
-
-// ///////////////////////////////////////////////////////////////////////////////
-// // printHeap
-// // prints the heap
-// ///////////////////////////////////////////////////////////////////////////////
-// void printHeap(Heap h)
-// {
-//   printf("HEAP: ");
-//   for (int i = 1; i <= h->n; i++)
-//   {
-//     printf("[%d: %s %d/%d] ", i, h->heap[i]->value->room, h->heap[i]->key,
-//       h->heap[i]->value->temp);
-//   }
-//   printf("\n");
-// }
-
-// struct room * findmin(Heap h)
-// {
-//   demand(!empty(h), "findmin not allowed on empty heap\n");
-//   return h->heap[1]->value;
-// }
-
-// ///////////////////////////////////////////////////////////////////////////////
-// // initheap
-// // Initializes a heap
-// ///////////////////////////////////////////////////////////////////////////////
-// Heap initheap()
-// {
-//   Heap h = malloc(sizeof(*h));
-//   h->n = 0;
-//   h->size = MAXHEAP;
-//   h->heap = malloc(h->size * sizeof(*(h->heap))); // An array of heapslot pointers
-//   for (int i = 0; i < h->size; i++)
-//   {
-//     h->heap[i] = malloc(sizeof(*(h->heap[i])));
-//     h->heap[i]->key = INT_MIN;
-//     h->heap[i]->value = NULL;
-//   }
-//   //h->heap[0]->key = INT_MIN;
-//   return h;
-// }
-
-// ///////////////////////////////////////////////////////////////////////////////
-// // swap
-// // Searches a graph starting at 'key' for a room on fire by using a
-// // depth-first search
-// ///////////////////////////////////////////////////////////////////////////////
-// void swap(heapslot * s1, heapslot * s2)
-// {
-//   if (debugflag)
-//     printf("SWAP: %d <=> %d\n", s1->key, s2->key);
-//   heapslot temp;
-//   temp = *s1;
-//   *s1 = *s2;
-//   *s2 = temp;
-// }
-
-// ///////////////////////////////////////////////////////////////////////////////
-// // insert
-// // Inserts a room onto the heap
-// ///////////////////////////////////////////////////////////////////////////////
-// void insert(Heap h, int key, struct room * r)
-// {
-//   (h->n)++;
-//   if (debugflag)
-//     printf("INSERT: %d, %s, %d ", key, r->room, r->temp);
-//   int cur, parent;
-//   while(h->n >= h->size)
-//   {
-//     h->size *= 2;
-//     h->heap = realloc(h->heap, sizeof(*(h->heap)) * h->size);
-//     if (debugflag)
-//     {
-//       printf("\nExpanding heap size to %d\n", h->size);
-//       printf("Realloc: %p\n", (void *)h->heap);
-//     }
-//     for (int i = (h->size)/2; i < h->size; i++)
-//     {
-//       h->heap[i] = malloc(sizeof(*(h->heap[i])));
-//       h->heap[i]->key = INT_MIN;
-//       h->heap[i]->value = NULL;
-//     }
-//     demand (h != 0, "malloc fail in insert\n");
-//   }
-//   cur = (h->n);
-//   h->heap[cur]->key = key;
-//   h->heap[cur]->value = r;
-//   parent = cur/2;
-//   while (h->heap[parent]->key > h->heap[cur]->key)
-//   {
-//     if (debugflag)
-//     {
-//       printf("\n--");
-//       printHeap(h);
-//     }
-//     swap(h->heap[parent], h->heap[cur]);
-//     cur = parent;
-//     parent = cur/2;
-//   }
-//   if (debugflag)
-//     printHeap(h);
-// }
-
-// ///////////////////////////////////////////////////////////////////////////////
-// // deletemin
-// // Deletes the minimum of the heap
-// ///////////////////////////////////////////////////////////////////////////////
-// void deletemin(Heap h)
-// {
-//   int cur, child;
-//   demand(! empty(h), "deletemin not allowed on empty heap\n");
-//   swap(h->heap[1], h->heap[h->n--]);
-//   cur = 1;
-//   child = 2;
-//   while (child <= h->n)
-//   {
-//     if (debugflag)
-//     {
-//       printf("DELETEMIN:\n--");
-//       printHeap(h);
-//     }
-//     if (child < h->n && h->heap[child+1]->key < h->heap[child]->key)
-//       child++;
-//     if (h->heap[cur]->key > h->heap[child]->key)
-//     {
-//       demand(child <= h->size, "falling past leaves\n");
-//       swap(h->heap[cur], h->heap[child]);
-//       cur = child;
-//       child = 2*cur;
-//     }
-//     else
-//       break;
-//   }
-//   if (debugflag)
-//     printHeap(h);
-// }
-
-// ///////////////////////////////////////////////////////////////////////////////
-// // destroyHeap
-// // Returns true if heap is empty
-// ///////////////////////////////////////////////////////////////////////////////
-// void destroyHeap(Heap h)
-// {
-//   if (debugflag)
-//     printf("DestroyHeap\n");
-//   for (int i = 0; i < (h->size); i++)
-//   {
-//     if (i <= h->n)
-//     {
-//       if (debugflag)
-//         printf("Freeing: %d\n", h->heap[i]->key);
-//       if (h->heap[i]->value)
-//       {
-//         free(h->heap[i]->value);
-//         h->heap[i]->value = NULL;//NEW
-//       }
-//     }
-//     free(h->heap[i]);
-//   }
-//   free(h->heap);
-//   free(h);
-// }
