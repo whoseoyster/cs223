cab00d7226fccfab73f713bf5fe311ec94503d1f
Rishab Ramanathan <rishabramanathan@Rishabs-MBP.wireless.yale.internal>
2017-04-16 18:27:26 -0400

    test

--- hw6/heaptest.c
+++ hw6/heaptest.c
@@ -6,7 +6,7 @@
 #include "/c/cs223/hw6/heap.h"
 #include "/c/cs223/hw6/dict.h"
 
-// Heap handles duplicates reasonably
+// findMin on empty heap
 
 int main(void){
 
@@ -19,54 +19,7 @@ int main(void){
     printf("Heap is empty\n");
   }
 
-  struct room * r;
-
-  // Insert six rooms
-  r = malloc(sizeof(struct room));
-  sprintf(r->room, "R12");
-  insert(h, 6, r);
-
-  r = malloc(sizeof(struct room));
-  sprintf(r->room, "R11");
-  insert(h, 3, r);
-
-  r = malloc(sizeof(struct room));
-  sprintf(r->room, "R12");
-  insert(h, 6, r);
-
-  r = malloc(sizeof(struct room));
-  sprintf(r->room, "R08");
-  insert(h, 1, r);
-
-  r = malloc(sizeof(struct room));
-  sprintf(r->room, "R09");
-  insert(h, 5, r);
-
-  r = malloc(sizeof(struct room));
-  sprintf(r->room, "R08");
-  insert(h, 1, r);
-
-  // Remove minimum
-  // You can assume that any heaptest will:
-      // 1. findMin
-      // 2. free room returned by findMin
-      // 3. deleteMin
-  // All three steps will occur, in that order, in any heaptest
-  // Thus, deleteMin is *not* responsible for freeing the room
-  r = findmin(h);
-  printf("room: %s\n", r->room);
-
-  free(r);
-  deletemin(h);
-
-  // check new minimum
-  r = findmin(h);
-  printf("room: %s\n", r->room);
-
-  // Check empty
-  if (empty(h)) {
-    printf("Heap is empty\n");
-  }
+  findmin(h);
 
   // Destroy the heap
   // destroyHeap *should* free not only the heap but also the rooms on the heap
